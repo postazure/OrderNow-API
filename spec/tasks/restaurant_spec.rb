fba517fdaacaf5d73c9165bfc895ab457563d631
Max Gordon <gordonmaxc@gmail.com>
2015-03-06 15:27:45 -0800

    setup vcr in helper

---
+++ spec/tasks/restaurant_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'rake'
+
+describe 'restaurant namespace rake task' do
+  before :all do
+    Rake.application.rake_require "tasks/restaurant"
+    Rake::Task.define_task(:environment)
+  end
+
+  describe "restaurant:get_core" do
+    let :run_rake_task do
+      Rake::Task["restaurant:get_core"].reenable
+      Rake.application.invoke_task "restaurant:get_core"
+    end
+
+    it ""
+
+  end
+end
