1aac395f0bbf78ceb0bc3c6135851637cb2ae8f3
Max Gordon <gordonmaxc@gmail.com>
2015-04-02 14:03:38 -0700

    ready for bluemix deploy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,14 @@
 if Rails.env.production?
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+  services = JSON.parse(ENV['VCAP_SERVICES'])
+  redis = services["redis-2.6"]
+  credentials = redis.first["credentials"]
+  host = credentials["host"]
+  port = credentials["port"]
+  password = credentials["password"]
+  db = credentials["name"]
+
+  # $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+  $redis = Redis.new(host: host, port:port, password:password, db:db)
 else
   $redis = Redis.new(host: "127.0.0.1", port: "6379")
 end
